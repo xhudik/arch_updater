822b5b3994f5250c98706a36ec348a6fc7bf3f37
xhudik <tomas.hudik@atlascopco.com>
2025-03-22 21:11:54 +0100

    update after reboot instead of regularly 11:00

--- arch_updater.sh
+++ arch_updater.sh
@@ -2,24 +2,24 @@
 
 echo "Archlinux system upgrade, `date +'%Y-%m-%dT%H:%M:%S'`"
 
-#check whether mobile data (hotspot Asus Zen8) is used
-mobile_data=`nmcli -t -f name connection show --active | grep "Asus Zen8" | wc -l`
+#check whether mobile data (hotspot Motorolka) is used
+mobile_data=`nmcli -t -f name connection show --active | grep "Motorolka" | wc -l`
 
 #will firefox be updated?
-update_firefox=`checkupdates | grep firefox | wc -l`
+#update_firefox=`checkupdates | grep firefox | wc -l`
 
 #if no match with mobile hot-spot
 if [[ ${mobile_data} -eq 0 ]]; then
 	echo "We are on WiFi - YES"
 	
-	if [[ ${update_firefox} -eq 0 ]]; then
-		#echo "Firefox will not be updated."
-		echo "I'm updating Arch linux. Running: 'pacman -Syu --noconfirm'"
+#	if [[ ${update_firefox} -eq 0 ]]; then
+#		#echo "Firefox will not be updated."
+#		echo "I'm updating Arch linux. Running: 'pacman -Syu --noconfirm'"
 		pacman -Syu --noconfirm
-	else
-		echo -e 'Firefox is about to be updated. This can not be done automatically!\nEXIT'
-		notify-send "ArchLinux is NOT updated" "Please run: pacman -Syu --noconfirm"
-	fi
+#	else
+#		echo -e 'Firefox is about to be updated. This can not be done automatically!\nEXIT'
+#		notify-send "ArchLinux is NOT updated" "Please run: pacman -Syu --noconfirm"
+#	fi
 else
-	echo -e 'Mobile data are being used (hotspot: Asus Zen8). Arch linux will not be updated\nEXIT'
+	echo -e 'Mobile data are being used (hotspot: Motorolka). Arch linux will not be updated\nEXIT'
 fi
